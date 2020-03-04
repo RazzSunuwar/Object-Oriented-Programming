855483044e1fc127be85504c6060c13843ea05d9
RazzSunuwar <razzkoinch5@gmail.com>
2020-03-04 21:17:04 +0545

    Second commit abstraction

--- abstraction.js
+++ abstraction.js
@@ -4,6 +4,12 @@ function Employee(name, age, baseSalary){
 	this.name = name;
 	this.age = age;
 	this.baseSalary = baseSalary;
+	this.monthlyBonus = 1000;
+
+	this.calculateFinalSalary = function(){
+		let finalSalary = this.baseSalary + this.monthlyBonus;
+		console.log('Final Saalary is : '+ finalSalary);
+	}
 
 	this.getEmpDetails = function(){
 		console.log('Name : '+this.name+' | Age : '+this.age);
@@ -12,3 +18,4 @@ function Employee(name, age, baseSalary){
 
 let em1 = new Employee('John', 30, 5000);
 em1.getEmpDetails();
+em1.calculateFinalSalary();
