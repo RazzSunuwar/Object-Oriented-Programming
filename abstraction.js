11e9851f42500d565b7e494e7c064fb4d5f6c99c
RazzSunuwar <razzkoinch5@gmail.com>
2020-03-04 21:27:53 +0545

    abstraction practice

--- abstraction.js
+++ abstraction.js
@@ -4,18 +4,18 @@ function Employee(name, age, baseSalary){
 	this.name = name;
 	this.age = age;
 	this.baseSalary = baseSalary;
-	this.monthlyBonus = 1000;
+	let monthlyBonus = 1000;
 
-	this.calculateFinalSalary = function(){
-		let finalSalary = this.baseSalary + this.monthlyBonus;
+	let calculateFinalSalary = function(){
+		let finalSalary = baseSalary + monthlyBonus;
 		console.log('Final Saalary is : '+ finalSalary);
 	}
 
 	this.getEmpDetails = function(){
 		console.log('Name : '+this.name+' | Age : '+this.age);
+		calculateFinalSalary();
 	};
 };
 
 let em1 = new Employee('John', 30, 5000);
 em1.getEmpDetails();
-em1.calculateFinalSalary();
